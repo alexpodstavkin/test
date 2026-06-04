d0daa24375aea2440290e772c8e549689c47efda
alexpodstavkin <apodstavkin2000@gmail.com>
2026-06-04 14:46:54 +0300

    add zerocoder-neuro-webinar landing (static export)

---
+++ zerocoder-neuro-webinar/_next/static/chunks/app/layout-783dd6673fda7aef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{1411:function(n,e,u){Promise.resolve().then(u.t.bind(u,8101,23))},8101:function(){}},function(n){n.O(0,[560,379,276,744],function(){return n(n.s=1411)}),_N_E=n.O()}]);
